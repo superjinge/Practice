5d90e56aefd5707c8a3c649457b002a77d6226d1
sp28 <superjinge28@gmail.com>
2022-11-21 21:22:28 +0800

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * java类作用描述
+ *
+ * @author chenjin02
+ * @date ${DATE}  ${TIME}
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
